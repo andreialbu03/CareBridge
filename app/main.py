811d5f400c3cb9a544d896a14b3a77c83bf9329b
Andrei Albu <andrei.albu03@gmail.com>
2024-01-17 10:32:35 -0500

    clean up code

--- app/main.py
+++ app/main.py
@@ -15,164 +15,35 @@ import re
 load_dotenv()
 logging.basicConfig(level=logging.INFO)
 
+# Environment variables
 GPT_API_KEY = os.getenv("GPT_API_KEY")
 AWS_ACCESS_KEY = os.getenv("AWS_ACCESS_KEY")
 AWS_SECRET_KEY = os.getenv("AWS_SECRET_KEY")
 AWS_REGION = os.getenv("AWS_REGION")
 S3_BUCKET = os.getenv("S3_BUCKET")
 
+# Initialize FastAPI app
 app = FastAPI()
-openai_client = OpenAI(
-    api_key=GPT_API_KEY
-)
+openai_client = OpenAI(api_key=GPT_API_KEY)
 
 # Initialize AWS Textract client
-textract_client = boto3.client('textract', aws_access_key_id=AWS_ACCESS_KEY, aws_secret_access_key=AWS_SECRET_KEY, region_name=AWS_REGION)
+textract_client = boto3.client(
+    "textract",
+    aws_access_key_id=AWS_ACCESS_KEY,
+    aws_secret_access_key=AWS_SECRET_KEY,
+    region_name=AWS_REGION,
+)
 
-# Serve static files (CSS, JS, etc.)
+# Serve static files (CSS, JS, etc.) and templates
 app.mount("/static", StaticFiles(directory="static"), name="static")
-
-# Templates
 templates = Jinja2Templates(directory="templates")
 
-# Route to render the homepage
-@app.get("/", response_class=HTMLResponse)
-async def read_item(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
 
-# Define a function to check the status of the Textract job
+# Function to check the status of the Textract job
 def check_textract_status(job_id):
     response = textract_client.get_document_text_detection(JobId=job_id)
     logging.info(f"Textract Status: {response['JobStatus']}")
-    return response['JobStatus']
-
-# Route to handle file uploads
-@app.post("/upload")
-async def create_upload_file(file: UploadFile = File(...)):
-    # Upload the file to S3
-    s3_client = boto3.client('s3', aws_access_key_id=AWS_ACCESS_KEY, aws_secret_access_key=AWS_SECRET_KEY, region_name=AWS_REGION)
-    try:
-        s3_client.upload_fileobj(file.file, S3_BUCKET, file.filename)
-        logging.info(f"Successfully uploaded file to S3: {file.filename}")
-    except NoCredentialsError:
-        raise HTTPException(status_code=500, detail="AWS credentials not available")
-    
-    # Use AWS Textract to extract text
-    response = textract_client.start_document_text_detection(
-        DocumentLocation={'S3Object': {'Bucket': S3_BUCKET, 'Name': file.filename}}
-    )
-
-    logging.info(f"Textract Response: {response}")
-
-    # Get the Textract job ID
-    job_id = response['JobId']
-
-    # Construct the URL for the result page
-    result_page_url = f"/result/{job_id}"
-
-    logging.info(f"Result page URL: {result_page_url}")
-
-    # Maximum number of polling retries
-    max_polling_retries = 20
-
-    # Initialize the polling retry count
-    polling_retry_count = 0
-
-    while polling_retry_count < max_polling_retries:
-        # Check the status of the Textract job
-        job_status = check_textract_status(job_id)
-
-        if job_status == 'SUCCEEDED':
-            # Textract job is complete, redirect to the result page
-            redirect_response = RedirectResponse(url=result_page_url)
-            return redirect_response
-        elif job_status == 'FAILED':
-            # Handle the case where the Textract job failed (optional)
-            return {"error": "Textract job failed"}
-        else:
-            # Textract job is still in progress, wait for a while and then poll again
-            polling_retry_count += 1
-            sleep(5)  # Wait for 5 seconds before polling again
-
-    # If all polling retries fail, you can handle the error or return a specific response
-    return {"error": "Failed to retrieve Textract results after multiple polling retries"}
-
-
-    # return RedirectResponse(url=result_page_url)
-
-@app.get("/result/{job_id}", response_class=HTMLResponse)
-@app.post("/result/{job_id}", response_class=HTMLResponse)
-async def get_result(request: Request, job_id: str):
-    try:
-        # Get the results from Textract
-        response = textract_client.get_document_text_detection(JobId=job_id)
-        
-        # Check if the Textract job is complete
-        job_status = response['JobStatus']
-        if job_status != 'SUCCEEDED':
-            raise HTTPException(status_code=500, detail=f"Textract job status: {job_status}")
-        
-        logging.info(f"Textract Response: {response['JobStatus']}")
-
-        # Extract and display text
-        blocks = response['Blocks']
-        # Save extracted text to a file
-        block_path = 'textract_blocks.json'
-        with open(block_path, 'w') as output_file2:
-            json.dump(blocks, output_file2, indent=2)
-
-        # extracted_text = ''.join([block['Text'] for block in blocks if block['BlockType'] == 'LINE'])
-        extracted_text = ''.join([block['Text'] if 'Text' in block and isinstance(block['Text'], str) else '' for block in blocks])
-        
-        # Save extracted text to a file
-        output_file_path = 'extracted_text.txt'
-        with open(output_file_path, 'w') as output_file:
-            output_file.write(extracted_text)
-
-
-    except NoCredentialsError:
-        raise HTTPException(status_code=500, detail="AWS credentials not available")
-    except Exception as e:
-        logging.error("Error processing Textract response: %s", str(e))
-        raise HTTPException(status_code=500, detail="Error processing Textract response")
-    
-
-    #####
-    # medical_entities = comprehend_medical(extracted_text)
-    # # Extract conditions, medications, anatomy, procedures, and other entities
-    # conditions = [entity['Text'] for entity in medical_entities if entity['Type'] == 'MEDICAL_CONDITION']
-    # medications = [entity['Text'] for entity in medical_entities if entity['Type'] == 'MEDICATION']
-    # anatomy = [entity['Text'] for entity in medical_entities if entity['Type'] == 'ANATOMY']
-    # procedures = [entity['Text'] for entity in medical_entities if entity['Type'] == 'TEST_TREATMENT_PROCEDURE']
-    # # Combine extracted entities into a summary
-    # summary = f"Conditions: {', '.join(conditions)}\nMedications: {', '.join(medications)}\nAnatomy: {', '.join(anatomy)}\nProcedures: {', '.join(procedures)}"
-    # print(medical_entities)
-
-    # Generate user-friendly explanation using GPT-3
-    explanation = generate_explanation(extracted_text)
-
-
-    return templates.TemplateResponse("result.html", {"request": request, "extracted_text": explanation})
-    # return templates.TemplateResponse("result.html", {"request": request, "extracted_text": explanation})
-
-
-# Function to extract medical entities using Comprehend Medical
-def comprehend_medical(text):
-    comprehend_medical = boto3.client('comprehendmedical', region_name=AWS_REGION, aws_access_key_id=AWS_ACCESS_KEY, aws_secret_access_key=AWS_SECRET_KEY)
-    response = comprehend_medical.detect_entities(Text=text)
-    medical_entities = response['Entities']
-    logging.info(f"Comprehend Medical Response Successful")
-
-    # Specify the file path where you want to save the output
-    output_file_path = 'comprehend_output.json'
-
-    # Open the file in write mode and write the extracted medical entities
-    with open(output_file_path, 'w') as output_file:
-        json.dump(medical_entities, output_file, indent=2)
-    
-    logging.info(f"Comprehend Medical output saved to {output_file_path}")
-
-    return medical_entities
+    return response["JobStatus"]
 
 
 # Function to generate user-friendly explanations using GPT-3
@@ -188,11 +59,112 @@ def generate_explanation(text):
     )
     explanation = response.choices[0].message.content
 
-    output_path = 'gpt_response.txt'
-    with open(output_path, 'w') as output_file:
-        output_file.write(explanation)
-
     return explanation
+
+
+# Route to render the homepage
+@app.get("/", response_class=HTMLResponse)
+async def read_item(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+# Route to handle file uploads
+@app.post("/upload")
+async def create_upload_file(file: UploadFile = File(...)):
+    # Upload the file to S3
+    s3_client = boto3.client(
+        "s3",
+        aws_access_key_id=AWS_ACCESS_KEY,
+        aws_secret_access_key=AWS_SECRET_KEY,
+        region_name=AWS_REGION,
+    )
+    try:
+        s3_client.upload_fileobj(file.file, S3_BUCKET, file.filename)
+        logging.info(f"Successfully uploaded file to S3: {file.filename}")
+    except NoCredentialsError:
+        raise HTTPException(status_code=500, detail="AWS credentials not available")
+
+    # Use AWS Textract to extract text
+    response = textract_client.start_document_text_detection(
+        DocumentLocation={"S3Object": {"Bucket": S3_BUCKET, "Name": file.filename}}
+    )
+    logging.info(f"Textract Response: {response}")
+
+    # Get the Textract job ID
+    job_id = response["JobId"]
+
+    # Construct the URL for the result page
+    result_page_url = f"/result/{job_id}"
+    logging.info(f"Result page URL: {result_page_url}")
+
+    # Maximum number of polling retries
+    max_polling_retries = 20
+    polling_retry_count = 0
+
+    while polling_retry_count < max_polling_retries:
+        # Check the status of the Textract job
+        job_status = check_textract_status(job_id)
+
+        if job_status == "SUCCEEDED":
+            # Textract job is complete, redirect to the result page
+            redirect_response = RedirectResponse(url=result_page_url)
+            return redirect_response
+        elif job_status == "FAILED":
+            # Handle the case where the Textract job failed
+            return {"error": "Textract job failed"}
+        else:
+            # Textract job is still in progress, wait for 5 sec and then poll again
+            polling_retry_count += 1
+            sleep(5)
+
+    # All polling retries fail
+    return {
+        "error": "Failed to retrieve Textract results after multiple polling retries"
+    }
+
+
+# Route to render the result page
+@app.get("/result/{job_id}", response_class=HTMLResponse)
+@app.post("/result/{job_id}", response_class=HTMLResponse)
+async def get_result(request: Request, job_id: str):
+    try:
+        # Get the results from Textract
+        response = textract_client.get_document_text_detection(JobId=job_id)
+
+        # Check again if the Textract job is complete
+        job_status = response["JobStatus"]
+        if job_status != "SUCCEEDED":
+            raise HTTPException(
+                status_code=500, detail=f"Textract job status: {job_status}"
+            )
+        logging.info(f"Textract Response: {response['JobStatus']}")
+
+        # Extract and display text
+        blocks = response["Blocks"]
+
+        extracted_text = "".join(
+            [
+                block["Text"]
+                if "Text" in block and isinstance(block["Text"], str)
+                else ""
+                for block in blocks
+            ]
+        )
+
+    except NoCredentialsError:
+        raise HTTPException(status_code=500, detail="AWS credentials not available")
+    except Exception as e:
+        logging.error("Error processing Textract response: %s", str(e))
+        raise HTTPException(
+            status_code=500, detail="Error processing Textract response"
+        )
+
+    # Generate user-friendly explanation using GPT-3
+    explanation = generate_explanation(extracted_text)
+
+    return templates.TemplateResponse(
+        "result.html", {"request": request, "extracted_text": explanation}
+    )
 
 
 if __name__ == "__main__":
