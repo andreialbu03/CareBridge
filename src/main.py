724177cfcb822c1a185557646b0f9002f1ec04ef
Andrei Albu <andrei.albu03@gmail.com>
2024-01-24 13:33:20 -0500

    update running app

--- src/main.py
+++ src/main.py
@@ -142,4 +142,4 @@ if __name__ == "__main__":
     import uvicorn
 
     # Run the FastAPI app using Uvicorn server
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
